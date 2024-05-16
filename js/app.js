a41fadbb5b43be0d901ca0b67415c4abd8fdfcb8
huzaifa8320 <muhammadhuzaifa8320@gmailcom>
2024-05-16 12:37:57 +0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 function change() {
     var random =Math.ceil(Math.random()*6)
+    document.getElementById("score").innerHTML = `${random} 🎈`
     var img = document.getElementById("img")
     img.src = `img/${random}.png`
 }
